6af33fe283f71348bd3a809f28d0cd2390a727ab
msouza <marcos.souza101907@gmail.com>
2025-05-31 11:23:56 -0300

    fixes qbittorrent.sh

--- setups/qbittorrent.sh
+++ setups/qbittorrent.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+print "Iniciando setup do radarr..."
+
+sleep 1
+print "3..."
+sleep 1
+print "2..."
+sleep 1
+print "1..."
+sleep 1
+
+war "Criando backup do arquivo de configuracao do qbittorrent..."
+
 checking_qbittorrent_credentials() {
     status_code=$(curl -s -o /dev/null -w "%{http_code}" -X POST "http://$qbittorrent_ip:8080/api/v2/auth/login" \
-    --data "username=$qbittorrent_user&password=$qbittorrent_pass" 2>/dev/null)
+    --data "username=$qbittorrent_user&password=$qbittorrent_pass_fixed" 2>/dev/null)
 
     if [ "$status_code" == "200" ]; then
         print "Credenciais do qbittorrent configuradas com sucesso!"
@@ -13,6 +25,31 @@ checking_qbittorrent_credentials() {
         exit 1
     fi
 }
+
+if [[ -f "configs/qBittorrent.conf.backup" ]]; then
+    print "Backup do arquivo de configuracao do qbittorrent encontrado: configs/qBittorrent.conf.backup"
+else
+    print "Criando backup do arquivo de configuracao do qbittorrent..."
+    cp configs/qBittorrent.conf configs/qBittorrent.conf.backup
+fi
+
+
+# creating backup of qbittorrent-data.conf
+if [[ -f "$qbittorrent_path/$data_path/qBittorrent.conf" ]]; then
+    print "Backup do arquivo de configuracao do qbittorrent encontrado: $qbittorrent_path/$data_path/qBittorrent.conf"
+    ask "Deseja sobrescrever o backup? [s/N]: "
+    if [[ "$input" =~ ^[sS]$ ]]; then
+        print "Sobrescrevendo backup do arquivo de configuracao do qbittorrent..."
+        cp "$qbittorrent_path/$data_path/qBittorrent.conf" "$backup_dir/qbittorrent/qBittorrent.conf"
+    else
+        print "Backup do arquivo de configuracao do qbittorrent não sobrescrito!"
+    fi
+else
+    war "Backup do arquivo de configuracao do qbittorrent não encontrado: $qbittorrent_path/$data_path/qBittorrent.conf"
+    print "Criando backup do arquivo de configuracao do qbittorrent..."
+    cp "$qbittorrent_path/$data_path/qBittorrent.conf" "$backup_dir/qbittorrent/qBittorrent.conf"
+fi
+
 
 print "Configurando o qbittorrent..."
 
@@ -37,3 +74,19 @@ else
 fi
 
 print "Configurado com sucesso!"
+
+if mv configs/qBittorrent.conf $qbittorrent_path/$data_path/qBittorrent.conf; then
+    print "Arquivo de configuracao do qbittorrent movido com sucesso!"
+    if checking_qbittorrent_credentials; then
+        print "Credenciais do qbittorrent configuradas com sucesso!"
+    else
+        err "Credenciais do qbittorrent não configuradas!"
+        err "Verifique se o qbittorrent está rodando e se as credenciais estão corretas"
+        err "Se o problema persistir, reinicie o container qbittorrent e verifique os logs"
+        exit 1
+    fi
+else
+    err "Erro ao mover o arquivo de configuracao do qbittorrent!"
+    err "Verifique se o arquivo de configuracao existe e se tem permissao de escrita"
+    exit 1
+fi
